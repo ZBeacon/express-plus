306e5eb2c33491c9a8d706a3fcb4a64d2991995f
Spyder <105814650+Spyder112@users.noreply.github.com>
2022-12-30 13:20:00 -0500

    maybe

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const server = require("express");
 const helmet = require("helmet");
 
-function lockdown(server, config = {}) {
+const { isArray } = Array;
+
+const helmetConfigs = [
+    "contentSecurityPolicy",
+    "crossOriginEmbedderPolicy",
+    "crossOriginOpenerPolicy",
+    "crossOriginResourcePolicy",
+    "dnsPrefetchControl",
+    "expectCt",
+    "frameguard",
+    "hidePoweredBy",
+    "hsts",
+    "ieNoOpen",
+    "noSniff",
+    "originAgentCluster",
+    "permittedCrossDomainPolicies",
+    "referrerPolicy",
+    "xssFilter"
+];
+
+function lockdown(server, config) {
+    if (!server) throw new Error("No server was provided.");
+
+    if (isArray(config)) {
+        for (let i = config.length; i--;) {
+            if (!helmetConfigs.includes(config[i])) throw new Error(`"${config[i]}" is not a valid helmet configuration.`);
+            server.use(helmet());
+        }
+        return;
+    }
     
     server.use(helmet());
-
 }
 module.exports = {
     lockdown: lockdown
