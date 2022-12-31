6cb8fda499fe79990fb8c30b7285de7e15ebace0
FlareZ <40903650+FlareZ123@users.noreply.github.com>
2022-12-30 19:02:52 -0500

    es2020 is good

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-const server = require("express");
 const helmet = require("helmet");
 
-const { isArray } = Array;
+const compression = require("compression");
 
 const helmetConfigs = [
     "contentSecurityPolicy",
@@ -21,16 +20,46 @@ const helmetConfigs = [
     "xssFilter"
 ];
 
-function lockdown(server, config) {
+const rateLimit = require('express-rate-limit');
+
+const limiter = rateLimit({
+	windowMs: 500, // 15 minutes
+	max: 35, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: false, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+const AntiDDoS = require("ddos");
+const { config } = require("process");
+
+const { ddos: ddosProtection } = new AntiDDoS({burst:15, limit:20});
+
+/**
+ * 
+ * @param {expressServer} server any express.js server
+ * @param {object} _config Object of settings, if not expressly stated, defaults will be used (activated)
+ * @returns 
+ */
+function lockdown(server, _config) {
     if (!server) throw new Error("No server was provided.");
 
-    if (isArray(config)) {
+    if (config?.ddosProtection) {
+        server.use(limiter); //stop ddoses
+        server.use(ddosProtection);
+    };
+
+    server.use(compression());
+
+    if (typeof _config === "object") {
+        let config = Object.entries(config);
         for (let i = config.length; i--;) {
-            if (!helmetConfigs.includes(config[i])) throw new Error(`"${config[i]}" is not a valid helmet configuration.`);
-            server.use(helmet());
-        }
+            if (!helmetConfigs.includes(config[i][0])) throw new Error(`"${config[i][0]}" is not a valid helmet configuration.`);
+            if (config[i][1]) {
+                server.use(helmet[config[i][0]]());
+            };
+        };
         return;
-    }
+    };
     
     server.use(helmet());
 }
